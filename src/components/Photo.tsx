e231c36f9f90161165e601c01725613602167cb7
SafieDeme9 <safiedeme9@gmail.com>
2026-01-20 21:41:46 +0100

    mobile responsiveness fixes

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,6 +1,6 @@
 export default function Photo() {
     return(
-        <div className="hidden lg:mt-0 md:col-span-5 lg:col-span-5 md:flex lg:flex object-cover">
+        <div className="hidden lg:mt-0 md:col-span-5 sm:col-span-5 lg:col-span-5 md:flex lg:flex object-cover">
             <img src="/images/safie.png" alt="Photo of Safietou" className="photo"/>
         </div>  
     );
